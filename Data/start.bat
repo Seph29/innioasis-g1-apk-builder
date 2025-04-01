47f140acdde68e962fc58013c32964684574670a
Thaidakar <130446887+Thydekar@users.noreply.github.com>
2025-04-01 13:23:55 -0700

    Update start.bat

--- Data/start.bat
+++ Data/start.bat
@@ -1,6 +1,8 @@
 @echo off
 setlocal enabledelayedexpansion
 
+cd /d %~dp0
+
 echo Please choose one item:
 echo ------------------------------------------------
 echo ------------------------------------------------
@@ -32,17 +34,33 @@ if "%input%"=="8" set "packageName=com.audible.application"
 if "%input%"=="9" set "packageName=com.amazon.kindle"
 if "%input%"=="10" set "packageName=com.flyersoft.moonreaderp"
 if "%input%"=="11" set "packageName=com.facebook.katana"
-if "%input%"=="12" set "packageName=com.android.chrome"echo %packageName% > .\proc\in.txt
-echo Apk will be renamed to %packageName%
-echo %packageName% > .\proc\in.txt
+if "%input%"=="12" set "packageName=com.android.chrome"
+
 if not defined packageName (
     echo Invalid selection. Please run the script again and choose a number between 1 and 12.
     pause
     exit /b
 )
 
+echo %packageName% > .\proc\in.txt
+echo Apk will be renamed to %packageName%
+
+echo Checking dependencies...
+if not exist .\bin\apktool.jar (
+    echo ERROR: apktool.jar not found in .\bin\
+    pause
+    exit /b
+)
+if not exist .\in\*.apk (
+    echo ERROR: No APK file found in .\in\
+    pause
+    exit /b
+)
+
 echo Decompiling APK...
-java -jar .\bin\apktool.jar d .\in\*.apk -o .\temp -f
+for %%f in (.\in\*.apk) do (
+    java -jar .\bin\apktool.jar d "%%f" -o .\temp -f
+)
 if %ERRORLEVEL% neq 0 (
     echo ERROR: Failed to decompile APK.
     pause
@@ -56,33 +74,3 @@ if %ERRORLEVEL% neq 0 (
     pause
     exit /b
 )
-
-echo Recompiling APK...
-java -jar .\bin\apktool.jar b .\temp -o .\out\unsigned.apk
-if %ERRORLEVEL% neq 0 (
-    echo ERROR: Failed to recompile APK.
-    pause
-    exit /b
-)
-
-echo Aligning APK...
-".\bin\zipalign.exe" -p -f -v 4 ".\out\unsigned.apk" ".\out\aligned.apk"
-if %ERRORLEVEL% neq 0 (
-    echo ERROR: Failed to align APK.
-    pause
-    exit /b
-)
-
-
-echo Signing APK...
-java -jar .\bin\apksigner.jar sign --key .\keys\testkey.pk8 --cert .\keys\testkey.x509.pem --out .\out\out.apk .\out\unsigned.apk
-
-del ".\out\aligned.apk"
-del ".\out\unsigned.apk"
-del ".\out\out.apk.idsig"
-
-
-
-echo APK recompiled, aligned, and signed successfully.
-pause
-explorer ".\out"
